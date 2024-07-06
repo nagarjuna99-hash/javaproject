72b0c70eef3f3a1eae021b686618aa213027da00
nagarjuna99-hash <76239796+nagarjuna99-hash@users.noreply.github.com>
2024-07-06 12:44:40 +0530

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,15 @@
+interface Project{
+
+  int a=10;
+  
+public void loginPage();
+public void creationPage();
+
+}
+
+class ProjectImpl implements Project{
+
+public void loginPage(){
+System.out.println("loginPage");
+}
+}
